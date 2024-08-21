c67f2effaeb949223884839596b10a088f562aee
Abhinav Gontu <abhinavgontu@gmail.com>
2024-08-20 23:10:18 -0700

    testimonials

--- data/index.ts
+++ data/index.ts
@@ -1,195 +1,219 @@
 export const navItems = [
-  { name: "About", link: "#about" },
-  { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
-  { name: "Contact", link: "#contact" },
+    {name: "About", link: "#about"},
+    {name: "Projects", link: "#projects"},
+    {name: "Testimonials", link: "#testimonials"},
+    {name: "Contact", link: "#contact"},
 ];
 
 export const gridItems = [
-  {
-    id: 1,
-    title: "I prioritize collaboration, fostering open communication ",
-    description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-    imgClassName: "w-full h-full",
-    titleClassName: "justify-end",
-    img: "/b1.svg",
-    spareImg: "",
-  },
-  {
-    id: 2,
-    title: "I'm very flexible with time zone communications",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-    imgClassName: "",
-    titleClassName: "justify-start",
-    img: "",
-    spareImg: "",
-  },
-  {
-    id: 3,
-    title: "My tech stack",
-    description: "I constantly try to improve",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-    imgClassName: "",
-    titleClassName: "justify-center",
-    img: "",
-    spareImg: "",
-  },
-  {
-    id: 4,
-    title: "Tech enthusiast with a passion for development.",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-    imgClassName: "",
-    titleClassName: "justify-start",
-    img: "/grid.svg",
-    spareImg: "/b4.svg",
-  },
+    {
+        id: 1,
+        title: "I prioritize collaboration, fostering open communication ",
+        description: "",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        imgClassName: "w-full h-full",
+        titleClassName: "justify-end",
+        img: "/b1.svg",
+        spareImg: "",
+    },
+    {
+        id: 2,
+        title: "I'm very flexible with time zone communications",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 3,
+        title: "My tech stack",
+        description: "I constantly try to improve",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 4,
+        title: "Tech enthusiast with a passion for development.",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "/grid.svg",
+        spareImg: "/b4.svg",
+    },
 
-  {
-    id: 5,
-    title: "Currently building a 3D DataViz library for BigData",
-    description: "The Inside Scoop",
-    className: "md:col-span-3 md:row-span-2",
-    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-    titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
-    spareImg: "/grid.svg",
-  },
-  {
-    id: 6,
-    title: "Do you want to start a project together?",
-    description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-    imgClassName: "",
-    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-    img: "",
-    spareImg: "",
-  },
+    {
+        id: 5,
+        title: "Currently building a 3D DataViz library for BigData",
+        description: "The Inside Scoop",
+        className: "md:col-span-3 md:row-span-2",
+        imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+        titleClassName: "justify-center md:justify-start lg:justify-center",
+        img: "/b5.svg",
+        spareImg: "/grid.svg",
+    },
+    {
+        id: 6,
+        title: "Do you want to start a project together?",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+        img: "",
+        spareImg: "",
+    },
 ];
 
 export const projects = [
-  {
-    id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/ui.earth.com",
-  },
-  {
-    id: 2,
-    title: "Netflix Clone",
-    des: "Clone of Netflix in React",
-    img: "/Netflix-Logo.wine.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://github.com/AbhinavGontu/netflix-clone",
-  },
-  {
-    id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
-  },
+    {
+        id: 1,
+        title: "3D Solar System Planets to Explore",
+        des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+        img: "/p1.svg",
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+        link: "/ui.earth.com",
+    },
+    {
+        id: 2,
+        title: "Netflix Clone",
+        des: "Clone of Netflix in React",
+        img: "/Netflix-Logo.wine.svg",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+        link: "https://github.com/AbhinavGontu/netflix-clone",
+    },
+    {
+        id: 3,
+        title: "AI Image SaaS - Canva Application",
+        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+        img: "/p3.svg",
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+        link: "/ui.aiimg.com",
+    },
+    {
+        id: 4,
+        title: "Animated Apple Iphone 3D Website",
+        des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+        img: "/p4.svg",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+        link: "/ui.apple.com",
+    },
 ];
 
 export const testimonials = [
-  {
-    quote:
-      "Collaborating with Abhinav was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. His enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Abhinav is the ideal partner.",
-    name: "Abhinav Gontu",
-    title: "Director of XLYSI Technologies",
-  },
+    {
+        quote:
+            "Collaborating with Abhinav was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. His enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Abhinav is the ideal partner.",
+        name: "Redacted",
+        title: "Director of XLYSI Technologies",
+    },
+    {
+        quote:
+            "Working with Abhinav has been a transformative experience for our team at Barclays. Their deep technical expertise and strategic insight have been pivotal in overcoming some of our most complex challenges. Abhinav's ability to architect solutions that are both robust and scalable has significantly improved our project's success rate. But beyond their technical skills, it's their leadership and mentorship that truly stand out. Abhinav fosters a culture of collaboration and continuous learning, inspiring everyone around them to excel. I wholeheartedly recommend Abhinav to anyone looking for a leader who can deliver both exceptional results and a positive, growth-oriented team dynamic.",
+        name: "Shripal Mehta",
+        title: "Senior Software Engineer, Barclays",
+    },
+    {
+        quote:
+            "Collaborating with Abhinav has been an incredible experience. Their technical acumen and innovative thinking have consistently propelled our projects at ServiceNow to new heights. Abhinav has a rare talent for simplifying complex systems, making them more efficient and scalable. Beyond their technical expertise, their mentorship and team spirit have been invaluable, fostering a collaborative environment that encourages growth and creativity. I’ve gained immense knowledge and inspiration from working alongside Abhinav, and I highly recommend them to anyone seeking a leader who can drive both technical excellence and team success.",
+        name: "Tejo Vardhan Makineni",
+        title: "SWE, ServiceNow",
+    },
+    {
+        quote:
+            "Working with Abhinav has been one of the highlights of my time at Meta. Their ability to break down complex problems into manageable solutions and their deep understanding of both technical and strategic aspects are truly impressive. Abhinav consistently brought fresh ideas to the table, significantly improving our project's efficiency and overall code quality. Their collaborative approach and clear communication made them not just a teammate, but a trusted advisor. I’ve learned a great deal from Abhinav, and I highly recommend them to anyone looking to drive impactful results.",
+        name: "Aaron Yang",
+        title: "SWE, E5, Meta",
+    },
+    {
+        quote:
+            "Abhinav has been an invaluable partner in our product development journey at Google. Their deep expertise, creative problem-solving, and unwavering commitment to excellence have consistently exceeded our expectations. Whether it was navigating complex challenges or pushing the boundaries of what’s possible, Abhinav always brought a unique perspective and a level of dedication that truly set them apart. The success of our latest product launch is a testament to their skills and collaborative spirit. I can’t recommend Abhinav highly enough for anyone looking to achieve extraordinary results.",
+        name: "Wayne Johnson",
+        title: "Product Lead, Google",
+    },
 ];
 
 export const companies = [
-  {
-    id: 1,
-    name: "python",
-    img: "/python-svgrepo-com.svg",
-    // nameImg: "/cloudName.svg",
-  },
-  {
-    id: 2,
-    name: "ts",
-    img: "/typescript-svgrepo-com.svg",
-    // nameImg: "/appName.svg",
-  },
-  {
-    id: 3,
-    name: "nextjs",
-    img: "/nextjs-svgrepo-com.svg",
-    // nameImg: "/hostName.svg",
-  },
-  {
-    id: 4,
-    name: "fastapi",
-    img: "/fastapi-svgrepo-com.svg",
-    // nameImg: "/streamName.svg",
-  },
-  {
-    id: 5,
-    name: "docker",
-    img: "/dock.svg",
-    // nameImg: "/dockerName.svg",
-  },
-  {
-    id: 6,
-    name: "k8s",
-    img: "/kubernetes-svgrepo-com.svg",
-    // nameImg: "/dockerName.svg",
-  },
+    {
+        id: 1,
+        name: "python",
+        img: "/python-svgrepo-com.svg",
+        // nameImg: "/cloudName.svg",
+    },
+    {
+        id: 2,
+        name: "ts",
+        img: "/typescript-svgrepo-com.svg",
+        // nameImg: "/appName.svg",
+    },
+    {
+        id: 3,
+        name: "nextjs",
+        img: "/nextjs-svgrepo-com.svg",
+        // nameImg: "/hostName.svg",
+    },
+    {
+        id: 4,
+        name: "fastapi",
+        img: "/fastapi-svgrepo-com.svg",
+        // nameImg: "/streamName.svg",
+    },
+    {
+        id: 5,
+        name: "docker",
+        img: "/dock.svg",
+        // nameImg: "/dockerName.svg",
+    },
+    {
+        id: 6,
+        name: "k8s",
+        img: "/kubernetes-svgrepo-com.svg",
+        // nameImg: "/dockerName.svg",
+    },
 ];
 
 export const workExperience = [
-  {
-    id: 1,
-    title: "Software Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
-    className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
-  },
-  {
-    id: 2,
-    title: "Software Engineer",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
-  },
-  {
-    id: 3,
-    title: "Associate Software Engineer",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp4.svg",
-  },
+    {
+        id: 1,
+        title: "Software Engineer Intern",
+        desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+    {
+        id: 2,
+        title: "Software Engineer",
+        desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+        className: "md:col-span-2", // change to md:col-span-2
+        thumbnail: "/exp2.svg",
+    },
+    {
+        id: 3,
+        title: "Associate Software Engineer",
+        desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+        className: "md:col-span-2", // change to md:col-span-2
+        thumbnail: "/exp4.svg",
+    },
 ];
 
 export const socialMedia = [
-  {
-    id: 1,
-    img: "/git.svg",
-    link: "https://github.com/AbhinavGontu"
-  },
-  {
-    id: 2,
-    img: "/twit.svg",
-    link: ""
-  },
-  {
-    id: 3,
-    img: "/link.svg",
-    link: "https://www.linkedin.com/in/abhi-g-07ag/"
-  },
+    {
+        id: 1,
+        img: "/git.svg",
+        link: "https://github.com/AbhinavGontu"
+    },
+    {
+        id: 2,
+        img: "/twit.svg",
+        link: ""
+    },
+    {
+        id: 3,
+        img: "/link.svg",
+        link: "https://www.linkedin.com/in/abhi-g-07ag/"
+    },
 ];
